b5605fb9d937791357826df0c33d26a142af3c98
Shontara Hatton <shontarahatton@Interapts-MacBook-Pro-6.local>
2018-01-23 15:44:40 -0500

    first commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Shontara Hatton on 1/23/18.
+//  Copyright © 2018 Shontara Hatton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
